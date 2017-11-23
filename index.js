d816ff357416f0d2160fbf967b46dc7fb9bd0183
Alexei Accio <a.tukachev@aledo-pro.ru>
2017-11-23 16:00:56 +0300

    pagination fix

--- index.js
+++ index.js
@@ -77,11 +77,11 @@ let bookPages = 100;
 
 function getPagination( current, maxpage ) {
   let keys = []
-  if (current>1) keys.push({ text: `«1`, callback_data: '1' })
-  if (current>2) keys.push({ text: `‹${current-1}`, callback_data: (current-1).toString() })
-  keys.push({ text: `-${current}-`, callback_data: current.toString() })
-  if (current<maxpage-1) keys.push({ text: `${current+1}›`, callback_data: (current+1).toString() })
-  if (current<maxpage) keys.push({ text: `${maxpage}»`, callback_data: maxpage.toString() })
+  if (current>1) keys.push({ text: `« 1`, callback_data: '1' })
+  if (current>2) keys.push({ text: `‹ ${current-1}`, callback_data: (current-1).toString() })
+  keys.push({ text: `· ${current} ·`, callback_data: current.toString() })
+  if (current<maxpage-1) keys.push({ text: `${current+1} ›`, callback_data: (current+1).toString() })
+  if (current<maxpage) keys.push({ text: `${maxpage} »`, callback_data: maxpage.toString() })
 
   return {
     reply_markup: JSON.stringify({
